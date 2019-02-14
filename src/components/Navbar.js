cb19a64de64ed94e5e34fa176705671cbaf82549
eabehr <emilyabehrendt@gmail.com>
2019-02-14 16:57:06 -0500

    Add DESC logo to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,19 @@ const Navbar = props => {
     return (
         <nav className="nav-wrapper teal">
             <div className="container">
+                <span className="desc-logo">
+                    <img src="https://www.desc.org/wp-content/themes/desc/img/logo-desc.png"
+                        height="60"
+                        width="60" />
+                </span>
                 <Link to="/" className="brand-logo">
-                    DESC Simplify
+                    DESC In Kind Portal
                 </Link>
                 {props.isAuth ? (
                     <SignedInLinks contextUser={props.contextUser} />
                 ) : (
-                    <SignedOutLinks />
-                )}
+                        <SignedOutLinks />
+                    )}
             </div>
         </nav>
     );
